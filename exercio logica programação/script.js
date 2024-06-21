75959bfc115503c5e924283cc832e79e7b4e22b5
mellodev11 <robertale45@gmail.com>
2024-06-21 14:54:25 -0300

    finalizando atividade de log programação com js

--- exercio logica programação/script.js
+++ exercio logica programação/script.js
@@ -1,14 +1,35 @@
-let numberOne = Number(prompt("digite o primeiro número:"))
-let numberTwo = Number(prompt("digite o segundo número:"))
+const numberOne = Number(prompt('Digite o primeiro número'))
+const numberTwo = Number(prompt('Digite o segundo número'))
 
+const som = numberOne + numberTwo
+const subtraction = numberOne - numberTwo
+const multiplication = numberOne * numberTwo
+const division = numberOne / numberTwo
+const rest = numberOne % numberTwo
+//PRIMEIRA FORMA DE FAZER COM STRING LITERALS
+// alert(`A soma dos valores é: ${som}
+//     a subtração é ${subtraction}
+//     a multiplicação é ${multiplication}
+//     a divisão é ${division}
+//     e o resto da divisão é ${rest}`)
 
-function sum(numberOne, numberTwo){
-    let sumN = numberOne + numberTwo
-    return sumN
-    
+//FORMA DO EXERCÍCIO PEDIDO
+   alert(`A soma é :${som}`) 
+   alert(`A subtração é :${subtraction}`) 
+   alert(`A multiplicação é :${multiplication}`) 
+   alert(`A divisão é :${division}`) 
+   alert(`O resto da divisão é :${rest}`) 
+   
+if (numberOne === numberTwo){
+alert("Números iguais")
+} else{
+    alert("Números diferentes")
 }
+    
+    
 
-let soma = sum(numberOne, numberTwo)
-alert(soma)
-
-
+if (som %2 == 0){
+    alert("O número é par")
+}else{
+    alert("O número é impar")
+}
